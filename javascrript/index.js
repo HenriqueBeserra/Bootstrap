c383431058b4ffe759010305c3dcaec40ef0781a
HenriqueBeserra <millerhiquinho@gmail.com>
2022-01-28 21:09:23 -0300

    Projeto Bootstrap Spiderman

---
+++ javascrript/index.js
@@ -0,0 +1,11 @@
+
+document.querySelector('#peter').addEventListener("click", () =>{
+    console.log('Seja bem vindo Peter')
+    document.querySelector('body').setAttribute('class','blue')
+  
+})
+document.querySelector('#spider').addEventListener("click", () =>{
+    console.log('Sr. Homem Aranha.')
+    document.querySelector('body').setAttribute('class','black')
+ 
+})  
